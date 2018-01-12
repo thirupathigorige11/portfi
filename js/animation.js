b887a1ac9c5e09a1d668a50a81e31716eb7bf32b
thirupathigorige11 <thirupathigorige@gmail.com>
2018-01-12 14:26:31 +0530

    portfi

---
+++ js/animation.js
@@ -0,0 +1,65 @@
+ window.sr = ScrollReveal({ reset: true });
+         sr.reveal('.navbar',{
+         duration:2000,
+         origin:'bottom'
+         });
+
+        sr.reveal('.ses1-left',{
+     duration:2000,
+     origin:'top',
+     distance:"300px"
+     });
+          sr.reveal('.ses1-right',{
+     duration:2000,
+     origin:'right'
+
+     });
+     sr.reveal('#ses2',{
+     duration:2000,
+     origin:'top',
+     distance:"600px"
+     });
+     sr.reveal('#ses3',{
+     duration:2000,
+     origin:'bottom',
+     distance:"300px"
+     });
+
+     sr.reveal('.ses2-right',{
+     duration:2000,
+     delay:3000
+     });
+     sr.reveal('#sespart1',{
+     duration:2000,
+     origin:'left'
+
+     });
+     sr.reveal('span',{
+     duration:2000,
+     origin:'top',
+     delay:1000,
+     distance:"300px"
+     });
+     sr.reveal('.tech',{
+     duration:2000,
+     origin:'right'
+     });
+      sr.reveal('.form-control',{
+     duration:2000,
+     origin:'right'
+     });
+       sr.reveal('#btnsend',{
+     duration:2000,
+     origin:'right'
+     });
+     
+       sr.reveal('.social-left',{
+     duration:2000,
+     origin:'left',
+     distance:"300px"
+     });
+      sr.reveal('.social-right',{
+     duration:2000,
+     origin:'right',
+     distance:"300px"
+     });
